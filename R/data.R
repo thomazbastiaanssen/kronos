273890edd98e81335eb0da3d3f1e458732ab3746
Thomaz Bastiaanssen <thomazbastiaanssen@gmail.com>
2022-12-14 14:37:42 +0000

    Added descriptions to demo data

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' Snippet of example data to demonstrate the functionality of kronos in the most simple scenario. 
+#'
+#' @format A long format data.frame object with three columns, and 31 rows, samples.
+#'
+"onevariable"
+
+#' Snippet of example data to demonstrate the functionality of kronos betwween and among three different groups  
+#'
+#' @format A long format data.frame object with three columns, and 94 rows, samples.
+#'
+"groupdata"
+
+#' Snippet of example data to demonstrate the functionality of kronos betwween and among three different groups  
+#'
+#' @format A long format data.frame object with three 113 columns, features, and 94 rows, samples.
+#'
+"bigdata"
+
+#' Descriptional metadata for the 'bigdata' object, for the purpose of demonstration. 
+#'
+#' @format A long format data.frame object with three 113 columns and 94 rows.
+#'
+"bigmeta"
