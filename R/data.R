ea2239ae5803334c9b5c8bccc950dee8d309935f
Sarah-Jane Leigh <sjleigh@ucc.ie>
2022-12-16 17:23:01 +0000

    Two-way 16/12/2022

--- R/data.R
+++ R/data.R
@@ -2,23 +2,26 @@
 #'
 #' @format A long format data.frame object with three columns, and 31 rows, samples.
 #' 
-#' @source Dummy data modified from XYZ in order to demonstrate YZ
+#' @source Dummy data modified from metagenomic analysis of mouse caecal contents over the day to demonstrate rhythmicity within one group.
 "onevariable"
 
 #' Snippet of example data to demonstrate the functionality of kronos between and among three different groups  
 #'
 #' @format A long format data.frame object with three columns, and 94 rows, samples.
 #'
+#' @source Dummy data modified from PCR analysis of mouse ileum over the day to demonstrate rhythmicity between multiple groups.
 "groupdata"
 
 #' Snippet of example data to demonstrate the functionality of kronos between and among three different groups  
 #'
 #' @format A long format data.frame object with three 113 columns, features, and 94 rows, samples.
 #'
+#'@source Dummy data modified from untargeted hippocampal metabolomics over the day to demonstrate rhythmicity between multiple groups in a large dataset.
 "bigdata"
 
 #' Descriptional metadata for the 'bigdata' object, for the purpose of demonstration. 
 #'
 #' @format A long format data.frame object with three 113 columns and 94 rows.
 #'
+#'@source Dummy metadata modified from untargeted hippocampal metabolomics over the day
 "bigmeta"
